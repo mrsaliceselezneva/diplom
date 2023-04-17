041e8076a82a1d6fe413a27b380e6343b42d355c
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2023-04-17 10:47:00 +0400

    mvc main

---
+++ src/pages/Main/MainConnector.js
@@ -0,0 +1,7 @@
+import MainController from "./MainController";
+
+function MainConnector() {
+    return <MainController />;
+}
+
+export default MainConnector;
---
+++ src/pages/Main/MainController.js
@@ -0,0 +1,7 @@
+import MainView from "./MainView";
+
+function MainController() {
+    return <MainView />;
+}
+
+export default MainController;
